e033cb7a05bedc6c6adb5ab0ca15ccedb9af225a
Eugene Golovnov <eugene.golovnov@whykingz.com>
2021-07-04 21:43:56 +0300

    app js tiny refactoring

--- app.js
+++ app.js
@@ -1,25 +1,30 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+// const createError = require('http-errors');
+// const path = require('path');
+// const cookieParser = require('cookie-parser');
+// const logger = require('morgan');
+const http = require('http');
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
 
-var app = express();
+// routes
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
 
-var http = require('http');
+const hostname = '127.0.0.1';
+const port = 3000;
 
-http.createServer(app).listen(app.get('port'), () => console.log('Cuddly Dollop express web server on port ' + app.get('port')));
-
-// Middleware
-app.use((req, res, next) => {
-  res.end('Hello, Cuddly Dollop!');
+const server = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello World');
 });
 
+server.listen(port, hostname, () => {
+  console.log(`Cuddly dollop server running at http://${hostname}:${port}/`);
+});
+
+
 // // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
+// app.set('views', path.join(__dirname, 'templates'));
 // app.set('view engine', 'ejs');
 
 // app.use(logger('dev'));
@@ -30,6 +35,8 @@ app.use((req, res, next) => {
 
 // app.use('/', indexRouter);
 // app.use('/users', usersRouter);
+
+
 
 // // catch 404 and forward to error handler
 // app.use(function(req, res, next) {
@@ -47,4 +54,4 @@ app.use((req, res, next) => {
 //   res.render('error');
 // });
 
-module.exports = app;
+module.exports = server;
